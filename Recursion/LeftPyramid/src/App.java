26596d1ba82feaa0f6d5e7267b2563d79fac6593
nickgaur <nikhilgaur3@rocketmail.com>
2022-06-19 21:02:02 +0530

    left pyramid

--- Recursion/LeftPyramid/src/App.java
+++ Recursion/LeftPyramid/src/App.java
@@ -1,5 +1,24 @@
 public class App {
+    static void leftPyramid(int n, int row) {
+        if (row > n) {
+            return;
+        }
+        printStar(row);
+        System.out.println();
+        leftPyramid(n, row + 1);
+    }
+
+    static void printStar(int noOfStars) {
+        if(noOfStars == 0){
+            return;
+        }
+        System.out.print("*");
+        printStar(noOfStars - 1);
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n = 0;
+        int row = 1;
+        leftPyramid(n, row);
     }
 }
