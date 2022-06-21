34962d1025a8d8e6f79def886ea8a7eba308b9bc
nickgaur <nikhilgaur3@rocketmail.com>
2022-06-22 03:24:51 +0530

    Dutch National Flag

--- Array/RotateAnArray/src/App.java
+++ Array/RotateAnArray/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int arr[] = { -1, -100, 3, 99 };
+        // int arr[] = { 1, 2, 3, 4, 5 };
         int k = 2;
 
         // NAIVE APPROACH
@@ -14,18 +15,25 @@ public class App {
 
         // EFFICIENT SOLUTION
         int tempArr[] = new int[arr.length - k];
-        for (int i = 0; i < tempArr.length; i++) {
+        for (int i = 0; i <= k; i++) {
             tempArr[i] = arr[i];
         }
+        // for (int elem : tempArr) {
+        // System.out.print(elem + " ");
+        // }
+        // System.out.println();
         for (int i = k + 1; i < arr.length; i++) {
             arr[i - k - 1] = arr[i];
         }
-        for (int i = 0; i < tempArr.length; i++) {
-            arr[i + k] = tempArr[i];
+        // for (int elem : arr) {
+        //     System.out.print(elem + " ");
+        // }
+        for (int i = 0; i <= k; i++) {
+        arr[i + k] = tempArr[i];
         }
 
         for (int elem : arr) {
-            System.out.print(elem + " ");
+        System.out.print(elem + " ");
         }
         System.out.println();
     }
