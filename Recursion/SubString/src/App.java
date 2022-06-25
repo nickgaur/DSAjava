19ee978a85060dfec91fd403702cb9415531632a
nickgaur <nikhilgaur3@rocketmail.com>
2022-06-26 00:45:02 +0530

    SubString

--- Recursion/SubString/src/App.java
+++ Recursion/SubString/src/App.java
@@ -1,8 +1,9 @@
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
 public class App {
-    static ArrayList<String> subSequence(String str){
-        if(str.length() == 0){
+    static ArrayList<String> subSequence(String str) {
+        if (str.length() == 0) {
             ArrayList<String> blank = new ArrayList<String>();
             blank.add("");
             return blank;
@@ -11,14 +12,15 @@ public class App {
         String subStr = str.substring(1);
         ArrayList<String> list = subSequence(subStr);
         ArrayList<String> result = new ArrayList<String>();
-        for(String elem: list){
+        for (String elem : list) {
             result.add(elem);
             result.add(singleChar + elem);
         }
         return result;
-    } 
+    }
+
     public static void main(String[] args) throws Exception {
-        String str = "nick";
+        String str = "abc";
         ArrayList<String> list = subSequence(str);
         System.out.print(list);
     }
